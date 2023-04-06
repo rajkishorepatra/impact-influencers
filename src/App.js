3e3e89e734202382a109ad7c2ef93beef114fbe7
Anurag Biswal <anugbswl@gmail.com>
2023-04-06 09:38:28 +0530

    react router setup and root template added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 // import logo from './logo.svg';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import './App.css';
 import Home from './pages/Home'
+import Root from "./pages/root";
+import ErrorPage from "./pages/error-page";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+        <Route index element={<Home />} />
+      </Route>
+      <Route path="login" element={<Login />}/>
+      <Route path="register" element={<Register/>} />
+    </>
+  )
+);
 
 function App() {
   return (
     <div className='App'>
-      <Home/>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/root.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Footer from '../components/footer'
+import NavBar from '../components/NavBar'
+
+// this is the root layout for the website
+// navbar and footer can be placed here and 
+// other elements will replace Outlet accoring to routes.
+
+export default function Root() {
+  return (
+    <>
+      <NavBar/>
+      <Outlet/>      
+      <Footer/>
+    </>
+  )
+}
