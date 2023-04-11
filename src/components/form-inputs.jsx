d8176838984912f51cbbb027e0addb4f02d01f0b
Anurag Biswal <anugbswl@gmail.com>
2023-04-11 18:58:21 +0530

    design input component

--- src/components/form-inputs.jsx
+++ src/components/form-inputs.jsx
@@ -1,26 +1,13 @@
-import { useField } from "formik";
 import Form from "react-bootstrap/Form";
 import "../css/forms.css";
 
-export const InputField = ({ label, ...props }) => {
-  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-  // which we can spread on <input>. We can use field meta to show an error
-  // message if the field is invalid and it has been touched (i.e. visited)
-  const [field, meta] = useField(props);
+export const InputField = ({ label,error, ...props }) => {
   return (
     <>
       <Form.Group className="mb-2">
-        <Form.Label htmlFor={props.id || props.name}>{label}</Form.Label>
-        <Form.Control
-          {...field}
-          {...props}
-          
-          isInvalid={meta.touched && meta.error}
-          isValid={meta.touched && !meta.error}
-        />
-        <Form.Control.Feedback type="invalid">
-          {meta.error}
-        </Form.Control.Feedback>
+        {label && <Form.Label htmlFor={props.id || props.name}>{label}</Form.Label>}
+        <Form.Control {...props} />
+        <Form.Control.Feedback type="invalid">{error}</Form.Control.Feedback>
       </Form.Group>
     </>
   );
