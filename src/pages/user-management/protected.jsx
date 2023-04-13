eb5fb3ecf7f65ba21b250cab804dff695ab3f63e
Anurag Biswal <anugbswl@gmail.com>
2023-04-14 02:14:02 +0530

    created component for protected route

--- src/pages/user-management/protected.jsx
+++ src/pages/user-management/protected.jsx
@@ -1,22 +1,13 @@
 import { UserAuth } from "../../context/AuthContext";
-import { Navigate, Outlet, useLocation } from "react-router-dom";
-
-// export  function PrivateRoute() {
-//   const location = useLocation();
-//   const { currentUser } = UserAuth();
-
-//   if (currentUser === undefined) {
-//     return null; // or loading indicator/spinner/etc
-//   }
-
-//   return currentUser ? (
-//     <Outlet />
-//   ) : (
-//     <Navigate to="/login" replace state={{ from: location }} />
-//   );
-// }
+import { Navigate, useLocation } from "react-router-dom";
 
 export function RequireAuth({ children }) {
   let { currentUser } = UserAuth();
-  return currentUser ? children : <Navigate to={'/login'} />;
+  let location = useLocation();
+  console.log("location", location);
+  return currentUser ? (
+    children
+  ) : (
+    <Navigate to={'/login'} state={{from: location}} replace />
+  );
 }
