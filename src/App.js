9760eb71a3aafcd801f89f75f036a250604a60d0
Anurag Biswal <anugbswl@gmail.com>
2023-04-14 02:13:13 +0530

    use browserrouter and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
   BrowserRouter,
-  Route,
-  Navigate,
-  useLocation,
-  Routes,
+  Route, Routes
 } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
@@ -14,52 +8,39 @@ import Root from "./pages/root";
 import ErrorPage from "./pages/error-page";
 import Login from "./pages/user-management/login";
 import Register from "./pages/user-management/register";
-import { AuthContextProvider, UserAuth } from "./context/AuthContext";
+import { AuthContextProvider } from "./context/AuthContext";
 import InfluencerProfile from "./pages/user-management/influencer";
-import { PrivateRoute, RequireAuth } from "./pages/user-management/protected";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
-        <Route index element={<Home />} />
-      </Route>
-      <Route path="login" element={<Login />} />
-      <Route path="register" element={<Register />} />
-      <Route
-        path="influencer"
-        element={
-          <RequireAuth>
-            <InfluencerProfile />
-          </RequireAuth>
-        }
-      />
-    </>
-  )
-);
+import { RequireAuth } from "./pages/user-management/protected";
+import Organization from "./pages/user-management/organization";
 
 function App() {
   return (
     <AuthContextProvider>
       <div className="App">
-
-        {/* <RouterProvider router={router} /> */}
         <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
-            <Route index element={<Home />} />
-          </Route>
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route
-            path="influencer"
-            element={
-              <RequireAuth>
-                <InfluencerProfile />
-              </RequireAuth>
-            }
-          />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+              <Route index element={<Home />} />
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route
+              path="influencer"
+              element={
+                <RequireAuth>
+                  <InfluencerProfile />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="org"
+              element={
+                <RequireAuth>
+                  <Organization />
+                </RequireAuth>
+              }
+            />
+          </Routes>
         </BrowserRouter>
       </div>
     </AuthContextProvider>
