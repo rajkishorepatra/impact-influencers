71b9a91450df16aaa74c3154a0cc25900d7a6590
Anurag Biswal <anugbswl@gmail.com>
2023-04-14 13:31:29 +0530

    pagenot found route added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import {
-  BrowserRouter,
-  Route, Routes
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Root from "./pages/root";
@@ -12,6 +9,7 @@ import { AuthContextProvider } from "./context/AuthContext";
 import InfluencerProfile from "./pages/user-management/influencer";
 import { RequireAuth } from "./pages/user-management/protected";
 import Organization from "./pages/user-management/organization";
+import PageNotFound from "./pages/page-not-found";
 
 function App() {
   return (
@@ -21,25 +19,27 @@ function App() {
           <Routes>
             <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
               <Route index element={<Home />} />
+              <Route
+                path="org"
+                element={
+                  <RequireAuth>
+                    <Organization />
+                  </RequireAuth>
+                }
+              />
+              <Route
+                path="influencer"
+                element={
+                  <RequireAuth>
+                    <InfluencerProfile />
+                  </RequireAuth>
+                }
+              />
             </Route>
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
-            <Route
-              path="influencer"
-              element={
-                <RequireAuth>
-                  <InfluencerProfile />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="org"
-              element={
-                <RequireAuth>
-                  <Organization />
-                </RequireAuth>
-              }
-            />
+
+            <Route path="*" element={<PageNotFound />} />
           </Routes>
         </BrowserRouter>
       </div>
