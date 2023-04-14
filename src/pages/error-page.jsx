bb14d0ae91fd006df0287a5e6aee43e9b87b117d
Anurag Biswal <anugbswl@gmail.com>
2023-04-14 13:02:32 +0530

    page not found page added

--- src/pages/error-page.jsx
+++ src/pages/error-page.jsx
@@ -1,18 +1,21 @@
-import { Button } from "react-bootstrap";
 import { Link, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
-    const error = useRouteError();
-    console.error(error);
-
-    return (
-        <div id="error-page" className="vh-100 d-flex flex-column align-items-center justify-content-center">
-            <h1 className="display-3">Oops! {error.status}</h1>
-            <p className="fs-4">Sorry, an unexpected error has occurred.</p>
-            <p>
-                {error.statusText || error.message}
-            </p>
-            <Link to="/" className="px-3 py-2 text-primary text-decoration-none rounded-2">Go to Homepage</Link>
-        </div>
-    );
+  const error = useRouteError();
+  return (
+    <div
+      id="error-page"
+      className="vh-100 d-flex flex-column align-items-center justify-content-center"
+    >
+      <h1 className="display-3">Oops! {error && error.status}</h1>
+      <p className="fs-4">Sorry, an unexpected error has occurred.</p>
+      <p>{error && error.message}</p>
+      <Link
+        to="/"
+        className="px-3 py-2 text-primary text-decoration-none rounded-2"
+      >
+        Go to Homepage
+      </Link>
+    </div>
+  );
 }
