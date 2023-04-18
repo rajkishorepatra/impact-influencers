0bfb3b4a0aa3dce029479222a79cae3b81d43e91
Anurag Biswal <anugbswl@gmail.com>
2023-04-19 03:22:34 +0530

    updated case study seciton

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,55 +1,68 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
-import { Scrollbar, Autoplay } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import CountUp from 'react-countup'
-import logo from '../assets/logo.jpg'
-import Testimonials from './Testimonials'
-import JoinUs from './JoinUs'
-import Campaign from './Campaign'
-import { SmallCarousel1, SmallCarousel2, SmallCarousel3 } from './SmallCarousel'
-
-import '../css/contents.css'
-import 'react-multi-carousel/lib/styles.css'
+import React from "react";
+import { Container } from "react-bootstrap";
+import CountUp from "react-countup";
+import Testimonials from "./Testimonials";
+import JoinUs from "./JoinUs";
+import Campaign from "./Campaign";
+import Cs1 from "../assets/cs1.png";
+import Cs2 from "../assets/cs2.png";
+import "../css/contents.css";
+import "react-multi-carousel/lib/styles.css";
 import "swiper/swiper-bundle.min.css";
 import "swiper/css";
 import "swiper/css/pagination";
 
-
-
 function Contents() {
-    return (
-        <div className='content pb-3'>
-            <Campaign/>
-            <Container>
-                <div className='count my-4 mb-5'>
-                    <div className='countSec p-3' style={{ borderLeft: "none" }}>
-                        <h1><CountUp end={20} duration={3} />+</h1>
-                        <h5>Projects Completed</h5>
-                        <p>From buttons, to inputs, navbars, alerts or cards, you are covered</p>
-                    </div>
-                    <div className='countSec p-3'>
-                        <h1>$<CountUp end={20000} duration={3} />+</h1>
-                        <h5>Fund raised</h5>
-                        <p>From buttons, to inputs, navbars, alerts or cards, you are covered</p>
-                    </div>
-                    <div className='countSec p-3'>
-                        <h1><CountUp end={30} duration={3} />+</h1>
-                        <h5>Influencers Engaged</h5>
-                        <p>From buttons, to inputs, navbars, alerts or cards, you are covered</p>
-                    </div>
-                    <div className='countSec p-3'>
-                        <h1><CountUp end={50000} duration={3} />+</h1>
-                        <h5>Reach</h5>
-                        <p>From buttons, to inputs, navbars, alerts or cards, you are covered</p>
-                    </div>
-                </div>
+  return (
+    <div className="content pb-3">
+      <Campaign />
+      <Container fluid>
+        {/* Numbers section */}
+        <div className="count my-4 mb-5">
+          <div className="countSec p-3" style={{ borderLeft: "none" }}>
+            <h1>
+              <CountUp end={20} duration={3} />+
+            </h1>
+            <h5>Projects Completed</h5>
+            <p>
+              From buttons, to inputs, navbars, alerts or cards, you are covered
+            </p>
+          </div>
+          <div className="countSec p-3">
+            <h1>
+              $<CountUp end={20000} duration={3} />+
+            </h1>
+            <h5>Fund raised</h5>
+            <p>
+              From buttons, to inputs, navbars, alerts or cards, you are covered
+            </p>
+          </div>
+          <div className="countSec p-3">
+            <h1>
+              <CountUp end={30} duration={3} />+
+            </h1>
+            <h5>Influencers Engaged</h5>
+            <p>
+              From buttons, to inputs, navbars, alerts or cards, you are covered
+            </p>
+          </div>
+          <div className="countSec p-3">
+            <h1>
+              <CountUp end={50000} duration={3} />+
+            </h1>
+            <h5>Reach</h5>
+            <p>
+              From buttons, to inputs, navbars, alerts or cards, you are covered
+            </p>
+          </div>
+        </div>
 
-
-
-                <div className='caseStudy mt-4'>
-                    <h1 className='mb-4' id='caseStudy'>Case Studies</h1>
-                    <Swiper
+        {/* Case Studies section */}
+        <div className="caseStudy mt-4">
+          <h1 className="mb-4" id="caseStudy">
+            Case Studies
+          </h1>
+          {/* <Swiper
                         className="mySwiper mb-5"
                         autoplay={{
                             delay: 4000,
@@ -110,11 +123,15 @@ function Contents() {
                                 </Col>
                             </Row>
                         </SwiperSlide>
-                    </Swiper>
-                </div>
+          </Swiper>  */}
+          <div className="my-2">          
+            {demoCaseStudies.map((item) => (
+              <CaseStudy key={item.id} {...item} />
+            ))}
+          </div>
+        </div>
 
-
-                {/* <div className='organization' id='organizations'>
+        {/* <div className='organization' id='organizations'>
                     <div className='flipCard'>
                         <div className="front">
                             <p>Our Partners</p>
@@ -181,13 +198,59 @@ function Contents() {
                     </div>
 
                 </div> */}
-                <hr />
-                <JoinUs />
-                <hr />
-                <Testimonials />
-            </Container>
-        </div>
-    )
+        <hr />
+        <JoinUs />
+        <hr />
+        <Testimonials />
+      </Container>
+    </div>
+  );
 }
 
-export default Contents
+export default Contents;
+
+function CaseStudy({ heading, data, img }) {
+  return (
+    <div className="cs-container d-flex justify-content-center my-4">
+      <div className="circle">
+        <div className="mx-2 py-4 ">
+          <h2 className="">{heading}</h2>
+          {data.map((p, index) => (
+            <p key={index}>{p}</p>
+          ))}
+        </div>
+        <img src={img} alt="case study" />
+      </div>
+    </div>
+  );
+}
+
+const demoCaseStudies = [
+  {
+    id: 1,
+    heading: "Healthcare",
+    data: [
+      "A is a non-profit organization that works to provide healthcare services to underprivileged communities. The organization was facing a shortage of medical equipment and supplies, making it difficult to provide adequate care.",
+      "They approached a corporate sponsor for help. The corporate sponsor provided a significant donation of medical equipment and supplies, allowing the organization to expand their services and reach more people in needis a non-profit organization that works to provide healthcare services to underprivileged communities.",
+    ],
+    img: Cs1,
+  },
+  {
+    id: 2,
+    heading: "Education",
+    data: [
+      "We are a non-profit organization that works to promote education for girls in developing countries. The organization was facing a challenge in reaching remote communities that lacked access to schools. They partnered with a telecommunications company to create a mobile education platform that could be accessed by students in these communities.",
+      " The platform provided educational content and resources, and also allowed students to communicate with teachers and other students online. As a result, the organization was able to reach more students and provide education to those who previously had limited access.",
+    ],
+    img: Cs2,
+  },
+  {
+    id: 3,
+    heading: "Education",
+    data: [
+      "We are a non-profit organization that works to promote education for girls in developing countries. The organization was facing a challenge in reaching remote communities that lacked access to schools. They partnered with a telecommunications company to create a mobile education platform that could be accessed by students in these communities.",
+      " The platform provided educational content and resources, and also allowed students to communicate with teachers and other students online. As a result, the organization was able to reach more students and provide education to those who previously had limited access.",
+    ],
+    img: Cs2,
+  },
+];
