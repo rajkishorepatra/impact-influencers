abe4efa8d8fdff55e59d0741982ab4021f21688a
Anurag Biswal <anugbswl@gmail.com>
2023-04-19 23:51:19 +0530

    database methods

---
+++ src/context/db-methods.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { db } from "../firebase";
+import { doc, setDoc } from "firebase/firestore"; 
+
+
+
+
+export function AddInfluencer(currentUser) {
+    const influencerRef = doc(db,"influencers",);
+  return <div></div>;
+}
