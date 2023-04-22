5a558cc4b56cdc00b52512251569ed124d6d2b6e
Jangyaseni666 <jangyasenisahoo17@gmail.com>
2023-04-22 18:09:52 +0530

    New modal added and UI changed

--- src/components/Campaign.js
+++ src/components/Campaign.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import logo from '../assets/logo.jpg';
+import img from '../assets/image.png';
+import { useState } from 'react';
+import InfoModal from './Modals/InfoModal';
 
 function Campaign() {
+    const [showInfo, setShowInfo] = useState(false);
     const responsive = {
         desktop: {
             breakpoint: { max: 3000, min: 900 },
-            items: 2
+            items: 3
         },
         mobile: {
             breakpoint: { max: 900, min: 0 },
@@ -15,35 +19,50 @@ function Campaign() {
         }
     };
     return (
-        <div className='caseStudy p-3'>
-            <h1 className='my-4' id='campaigns'>Our Campaigns</h1>
-            <Carousel responsive={responsive}>
-                <div className='px-2'>
-                    <div className='camp_data m-auto p-3 rounded' >
-                        <img src={logo} alt='..' />
-                        <p className='mt-3'>From buttons, to inputs, navbars, alerts or cards, you are coveredFrom buttons, to inputs, navbars, alerts or cards, you are coveredFrom buttons, to inputs, navbars, alerts or cards, you are covered</p>
+        <>
+        <div className='caseStudy p-3 car'>
+            <h1 className='my-4' id='campaigns'>Live Campaigns</h1>
+            <Carousel responsive={responsive} partialVisbile={false}>
+                <div class='img__wrap'>
+                    <img src={img} alt='dummy'/>
+                    <div class='divp'>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate numquam sint sunt totam facilis? Consequatur.</p>
+                        <a href='javascript:void(0)' onClick={() => setShowInfo(true)}>See More</a>
+                    </div> 
+                </div>
+                <div class='img__wrap'>
+                    <img src={img} alt='dummy'/>
+                    <div class='divp'>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate numquam sint sunt totam facilis? Consequatur.</p>
+                        <a href='javascript:void(0)' onClick={() => setShowInfo(true)}>See More</a>
                     </div>
                 </div>
-                <div className='px-2'>
-                    <div className='camp_data m-auto p-3 rounded' >
-                        <img src={logo} alt='..'  />
-                        <p className='mt-3'>From buttons, to inputs, navbars, alerts or cards, you are coveredFrom buttons, to inputs, navbars, alerts or cards, you are coveredFrom buttons, to inputs, navbars, alerts or cards, you are covered</p>
+                <div class='img__wrap'>
+                    <img src={img} alt='dummy'/>
+                    <div class='divp'>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate numquam sint sunt totam facilis? Consequatur.</p>
+                        <a href='javascript:void(0)' onClick={() => setShowInfo(true)}>See More</a>
                     </div>
                 </div>
-                <div className='px-2'>
-                    <div className='camp_data m-auto p-3  rounded' >
-                        <img src={logo} alt='..' />
-                        <p className='mt-3'>From buttons, to inputs, navbars, alerts or cards, you are coveredFrom buttons, to inputs, navbars, alerts or cards, you are coveredFrom buttons, to inputs, navbars, alerts or cards, you are covered</p>
+                <div class='img__wrap'>
+                    <img src={img} alt='dummy'/>
+                    <div class='divp'>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate numquam sint sunt totam facilis? Consequatur.</p>
+                        <a href='javascript:void(0)' onClick={() => setShowInfo(true)}>See More</a>
                     </div>
                 </div>
-                <div className='px-2'>
-                    <div className='camp_data rounded m-auto p-3' >
-                        <img src={logo} alt='..' />
-                        <p className='mt-3'>From buttons, to inputs, navbars, alerts or cards, you are coveredFrom buttons, to inputs, navbars, alerts or cards, you are coveredFrom buttons, to inputs, navbars, alerts or cards, you are covered</p>
+                <div class='img__wrap'>
+                    <img src={img} alt='dummy'/>
+                    <div class='divp'>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate numquam sint sunt totam facilis? Consequatur.</p>
+                        <a href='javascript:void(0)' onClick={() => setShowInfo(true)}>See More</a>
                     </div>
                 </div>
             </Carousel>
         </div>
+
+        <InfoModal show={showInfo} close={()=>setShowInfo(false)} />
+    </>
     )
 }
 
---
+++ src/components/Modals/InfoModal.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+export default function InfoModal(props) {
+    return (
+        <>
+            <Modal show={props.show} onHide={props.hide}>
+                <Modal.Header closeButton>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={props.close}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )}
