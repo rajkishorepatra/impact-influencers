93f5bcbc3ca0548c9f39185bdb4bb0653c78e5d8
Jangyaseni666 <jangyasenisahoo17@gmail.com>
2023-04-23 11:18:14 +0530

    ui changes made

--- src/components/Modals/InfoModal.js
+++ src/components/Modals/InfoModal.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import {React} from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
 export default function InfoModal(props) {
+
     return (
         <>
-            <Modal show={props.show} onHide={props.hide}>
+            <Modal show={props.show} onHide={props.close}>
                 <Modal.Header closeButton>
                 </Modal.Header>
                 <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
