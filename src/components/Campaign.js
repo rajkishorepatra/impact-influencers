e304f46535499152b4fb920f33479f3b51c1162b
Anurag Biswal <anugbswl@gmail.com>
2023-04-26 21:04:54 +0530

    improved the live campaigns

--- src/components/Campaign.js
+++ src/components/Campaign.js
@@ -1,74 +1,120 @@
-import React from 'react';
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import logo from '../assets/logo.jpg';
-import img from '../assets/image.png';
-import { useState } from 'react';
-import InfoModal from './Modals/InfoModal';
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+// import required modules
+import { Navigation, Pagination } from "swiper";
+import "../css/campaign.css";
+import "swiper/css/navigation";
+import Img from "../assets/image.png";
 
 function Campaign() {
-    const [showInfo, setShowInfo] = useState(false);
-    const responsive = {
-        desktop: {
-            breakpoint: { max: 3000, min: 900 },
-            items: 3
-        },
-        mobile: {
-            breakpoint: { max: 900, min: 0 },
-            items: 1
-        }
-    };
-    return (
-        <>
-        <div className='caseStudy p-3 car'>
-            <h1 className='my-4' id='campaigns'>Live Campaigns</h1>
-            <Carousel responsive={responsive} partialVisbile={false}>
-                <div class='img__wrap'>
-                    <img src={img} alt='dummy'/>
-                    <div class='divp'>
-                        <p>{'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate numquam sint sunt totam facilis? Consequatur.\n'}
-                        <a href='javascript:void(0)' onClick={() => setShowInfo(true)}>See More</a>
-                        </p>
-                    </div> 
-                </div>
-                <div class='img__wrap'>
-                    <img src={img} alt='dummy'/>
-                    <div class='divp'>
-                        <p>{'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate numquam sint sunt totam facilis? Consequatur.\n'}
-                        <a href='javascript:void(0)' onClick={() => setShowInfo(true)}>See More</a>
-                        </p>
-                    </div>
-                </div>
-                <div class='img__wrap'>
-                    <img src={img} alt='dummy'/>
-                    <div class='divp'>
-                        <p>{'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate numquam sint sunt totam facilis? Consequatur.\n'}
-                        <a href='javascript:void(0)' onClick={() => setShowInfo(true)}>See More</a>
-                        </p>
-                    </div>
-                </div>
-                <div class='img__wrap'>
-                    <img src={img} alt='dummy'/>
-                    <div class='divp'>
-                        <p>{'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate numquam sint sunt totam facilis? Consequatur.\n'}
-                        <a href='javascript:void(0)' onClick={() => setShowInfo(true)}>See More</a>
-                        </p>
-                    </div>
-                </div>
-                <div class='img__wrap'>
-                    <img src={img} alt='dummy'/>
-                    <div class='divp'>
-                        <p>{'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate numquam sint sunt totam facilis? Consequatur.\n'}
-                        <a href='javascript:void(0)' onClick={() => setShowInfo(true)}>See More</a>
-                        </p>
-                    </div>
-                </div>
-            </Carousel>
+  return (
+    <>
+      <div className="p-2">
+        <h1 className="my-4" id="campaigns">
+          Live Campaigns
+        </h1>
+        <Swiper
+          navigation={true}
+          modules={[Navigation, Pagination]}
+          pagination={{ clickable: true }}
+          className="mySwiper"
+          breakpoints={{
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            1200: {
+              slidesPerView: 3,
+              spaceBetween: 10,
+            },
+          }}
+        >
+          {campaigns.map((campaign) => (
+            <SwiperSlide key={campaign.id}>
+              <CampaignCard campaign={campaign} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="my-3 d-flex justify-content-center">
+          <a href="#" className="btn btn-primary">
+            view more
+          </a>
         </div>
-
-        <InfoModal show={showInfo} close={()=>setShowInfo(false)} />
+      </div>
     </>
-    )
+  );
 }
 
-export default Campaign
+export default Campaign;
+
+function CampaignCard({ campaign }) {
+  return (
+    <div className="camp-card">
+      <div className="camp-card-image">
+        <img src={Img} alt={campaign.title} />
+      </div>
+      <div className="camp-card-content">
+        <h3 className="h4">{campaign.title}</h3>
+        <div className="card-details">
+          <p className="h6 fw-1">{campaign.keyMessage}</p>
+          <a href={campaign.link} className="btn btn-secondary">
+            Learn More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const campaigns = [
+  {
+    id: 1,
+    title: "New Clothing Line",
+    image: "https://via.placeholder.com/300x200.png?text=Clothing+Line",
+    keyMessage:
+      "Be the first to rock our new collection! Shop now and get 20% off your first purchase.",
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "Software Company",
+    image: "https://via.placeholder.com/300x200.png?text=Software+Company",
+    keyMessage:
+      "Introducing our latest software designed to help small businesses increase productivity and streamline operations.",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "Health and Wellness Brand",
+    image: "https://via.placeholder.com/300x200.png?text=Health+and+Wellness",
+    keyMessage:
+      "Take your fitness and wellness routine to the next level with our all-natural supplements.",
+    link: "#",
+  },
+  {
+    id: 4,
+    title: "Travel Company",
+    image: "https://via.placeholder.com/300x200.png?text=Travel+Company",
+    keyMessage:
+      "Ready for your next adventure? Explore the world with us and get $100 off your first booking.",
+    link: "#",
+  },
+  {
+    id: 5,
+    title: "Online Course",
+    image: "https://via.placeholder.com/300x200.png?text=Online+Course",
+    keyMessage:
+      "Unlock your potential and learn new skills with our online courses. Sign up today!",
+    link: "#",
+  },
+  {
+    id: 6,
+    title: "Food Delivery Service",
+    image: "https://via.placeholder.com/300x200.png?text=Food+Delivery",
+    keyMessage:
+      "Get delicious meals delivered straight to your door. Order now and enjoy 20% off your first order!",
+    link: "#",
+  },
+];
