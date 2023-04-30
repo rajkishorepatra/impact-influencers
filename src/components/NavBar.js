58c688bab3dc8ca3695e65bdfd0652d59dc8253c
Jangyaseni666 <jangyasenisahoo17@gmail.com>
2023-04-30 11:19:12 +0530

    navbar and footer changed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,18 +7,21 @@ import InfluencerModal from './Modals/InfluencerModal';
 import OrgModal from './Modals/OrgModal';
 import CampModal from './Modals/CampModal';
 
+
+
+
 function NavBar() {
     const [showInflu, setShowInflu] = useState(false);
     const [showOrg, setShowOrg] = useState(false);
     const [showCamp, setShowCamp] = useState(false);
     return (
         <>
-            <Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark" style={{ width: '95%', borderRadius: '10px', top: '5px', zIndex: "100" }} className='m-auto'>
+            <Navbar collapseOnSelect fixed="top" expand="lg" variant='dark' style={{ width: '95%', borderRadius: '10px', top: '5px', zIndex: "100"}} className='m-auto navbar'>
                 <Container>
                     <Navbar.Brand href="#home">Impact Influencers</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-                        <Nav style={{ color: 'white' }}>
+                        <Nav>
                             {/* <Nav.Link href="#about-us">About Us</Nav.Link> */}
                             <Nav.Link href="#campaigns" className='mx-2'>Campaigns</Nav.Link>
                             <Nav.Link href="#caseStudy" className='mx-2'>Case Study</Nav.Link>
@@ -27,7 +30,7 @@ function NavBar() {
                                 <NavDropdown.Item href="#" onClick={() => setShowOrg(true)}>As Organization</NavDropdown.Item>
                                 <NavDropdown.Item href="#" onClick={() => setShowCamp(true)}>Start Campaign</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link href="#testimonials" className='mx-2'>Subscribe</Nav.Link>
+                            <Nav.Link href="#subscribe" className='mx-2'>Subscribe</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
