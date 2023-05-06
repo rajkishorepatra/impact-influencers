88b989c57e3695bcb0a1babeb15075057b10852c
Anurag Biswal <anugbswl@gmail.com>
2023-05-07 01:10:07 +0530

    .

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,57 +13,62 @@ import "swiper/css/navigation";
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper";
 
-function Main() {
+export default function Main() {
   const videoRef = useRef(null);
   const carouselRef = useRef(null);
-
+  const swiperslide = useRef(null);
   useEffect(() => {
-    console.log(videoRef.current);
-    console.log(videoRef.current.offsetHeight);
-    setTimeout(() => {
-      carouselRef.current.swiper.update();
-      }, 1000);
-    
+    carouselRef.current.swiper.on("slideChange", () => {
+      carouselRef.current.style.height = videoRef.current.offsetHeight + "px";
+      console.log(carouselRef.current);
+      // swiperslide.current.style.height = videoRef.current.offsetHeight + "px";
+    });
   }, []);
-  
 
   return (
     <>
       <div className="header">
         <div className="header-wrapper">
-          <div className=" header-contents d-flex flex-column flex-lg-row">
-            <div className="vid-container">
-              <video autoPlay loop muted className="ratio" ref={videoRef}>
-                <source
-                  src={StockVideo}
-                  type="video/mp4"
-                  className="header-video"
-                />
-              </video>
+          <div className=" row m-0 p-0">
+            <div className="col-12 col-lg-8 m-0 p-0">
+              <div className="vid-container">
+                <video autoPlay loop muted className="ratio" ref={videoRef}>
+                  <source
+                    src={StockVideo}
+                    type="video/mp4"
+                    className="header-video"
+                  />
+                </video>
+              </div>
             </div>
 
-            <Swiper
-              ref={carouselRef}
-              slidesPerView={1}
-              centeredSlides={true}
-              autoplay={{
-                delay: 2500,
-                disableOnInteraction: false,
-              }}
-              pagination={{
-                clickable: true,
-              }}
-              direction="vertical"
-              navigation={false}
-              loop={true}
-              modules={[Autoplay, Pagination, Navigation]}
-            >
-              {Influencers.map((i, index) => (
-                <SwiperSlide key={index}>
-                  <InfluencerCard name={i.name} bio={i.bio} />
-                </SwiperSlide>
-              ))}
-            </Swiper>
+            <div className="col-12 col-lg-4 m-0 p-0">
+              <Swiper
+                id="header-carousel"
+                ref={carouselRef}
+                height={500}
+                slidesPerView={1}
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false,
+                }}
+                pagination={{
+                  clickable: true,
+                }}
+                direction="vertical"
+                // navigation={false}
+                loop={true}
+                modules={[Autoplay, Pagination, Navigation]}
+              >
+                {Influencers.map((i, index) => (
+                  <SwiperSlide key={index} ref={swiperslide}>
+                    <InfluencerCard name={i.name} bio={i.bio} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
           </div>
         </div>
       </div>
@@ -71,21 +76,17 @@ function Main() {
   );
 }
 
-export default Main;
-
 function InfluencerCard({ name, bio, Pimg }) {
   return (
     <>
-      <div className="influencer-card d-flex">
-        <div className=" influencer-img bg-warning d-flex justify-content-end">
-          <img
-            src={
-              "https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg"
-            }
-            alt="influencer profile"
-          />
-        </div>
-        <div className="influencer-details bg-info text-start p-3 d-none">
+      <div className="influencer-card ">
+        <img
+          src={
+            "https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg"
+          }
+          alt="influencer profile"
+        />
+        <div className="influencer-details bg-info text-start p-3 ">
           <h3 className=" h3 fw-semibold ">{name}</h3>
           <p className=" text-muted p-0 text-wrap">
             {bio.lenght < 50 ? bio : bio.substr(0, 50) + "..."}
